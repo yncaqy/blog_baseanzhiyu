fffa497aa42aeb98ce3244b11acf7f726eabbcda
yncaqy <yncaqy@ync.com>
2024-11-03 21:17:08 +0800

    Site updated: 2024-11-03 21:17:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/30/hello-world/","2024/10/29/线性代数的解法/","2023/04/14/DRF/"];function toRandomPost(){
+var posts=["2024/10/30/hello-world/","2023/04/14/DRF/","2024/10/29/线性代数的解法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
