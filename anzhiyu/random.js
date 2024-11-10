dffa8ae653b6438e6fa7cbd4be6611bfb6e0cb16
yncaqy <yncaqy@ync.com>
2024-11-10 14:20:38 +0800

    Site updated: 2024-11-10 14:20:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["4046760165/","1243066710/","1885882428/","668177610/","3481172980/","1/"];function toRandomPost(){
+var posts=["1243066710/","4046760165/","1885882428/","668177610/","3481172980/","1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
