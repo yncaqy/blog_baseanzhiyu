30978faf73bf2ca3a9dbb34b5d6a568d9dc3397e
yncaqy <yncaqy@ync.com>
2024-12-10 21:03:57 +0800

    Site updated: 2024-12-10 21:03:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["1243066710/","4046760165/","3109476237/","668177610/","3481172980/","1885882428/","1/"];function toRandomPost(){
+var posts=["1243066710/","4046760165/","1885882428/","3109476237/","668177610/","3481172980/","1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
